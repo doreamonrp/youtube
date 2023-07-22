d65f2b9971427d37dc8b6b8e89f22ba82bdb290c
Hey there <doreamonrp@gmail.com>
2023-07-22 13:56:09 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppContext } from './context/contextApi';
+import Header from "./components/Header";
+import LeftNav from "./components/LeftNav";
+import Feed from "./components/Feed"
+import SeachResult from "./components/SearchResult";
+import VideoDetails from "./components/VideoDetails";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <AppContext>
+        <BrowserRouter>
+            <div className='flex flex-col h-full'>
+                <Header/>
+                <Routes>
+                    <Route path = "/" element = {<Feed />}/>
+                    <Route path = "/searchResult/:searchQuery" element = {<SeachResult />}/>
+                    <Route path = "/video/:id" element = {<VideoDetails />}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+        
+    </AppContext>
+  )
 }
 
 export default App;
